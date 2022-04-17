58cd8a8c06f657639aee38355142ee12928029ff
hasibzunair <hasibzunair@gmail.com>
2022-04-16 22:09:15 -0400

    updated segmentation pipeline documentation and wrap crop_with_hand_entropy_seg into a script

---
+++ test/segmenter.py
@@ -0,0 +1,240 @@
+import torch
+import torch.nn as nn
+from torchvision import datasets, transforms, utils
+import torch.hub
+import torchvision
+import numpy as np
+
+from PIL import Image
+from skimage.util import img_as_ubyte
+from skimage.color import rgb2gray
+from skimage.filters.rank import entropy
+from skimage.morphology import disk
+
+#To use this block, run `pip install pytorch-lightning`
+#Reference: https://github.com/guglielmocamporese/hands-segmentation-pytorch
+
+torch.manual_seed(0)
+
+if torch.cuda.is_available():
+    torch.backends.cudnn.deterministic = True
+
+# Device
+DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print("Using: ", DEVICE)
+
+
+# Full path to the product segmentation model
+model_path = '/home/hasib/product-segmentation/logs/unet_aicityt4/unet_aicityt4.pth'
+
+
+########### Product segmentation model ###########
+""" Convolutional block:
+    It follows a two 3x3 convolutional layer, each followed by a batch normalization and a relu activation.
+"""
+class conv_block(nn.Module):
+    def __init__(self, in_c, out_c):
+        super().__init__()
+
+        self.conv1 = nn.Conv2d(in_c, out_c, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(out_c)
+
+        self.conv2 = nn.Conv2d(out_c, out_c, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(out_c)
+
+        self.relu = nn.ReLU()
+
+    def forward(self, inputs):
+        x = self.conv1(inputs)
+        x = self.bn1(x)
+        x = self.relu(x)
+
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu(x)
+
+        return x
+
+""" Encoder block:
+    It consists of an conv_block followed by a max pooling.
+    Here the number of filters doubles and the height and width half after every block.
+"""
+class encoder_block(nn.Module):
+    def __init__(self, in_c, out_c):
+        super().__init__()
+
+        self.conv = conv_block(in_c, out_c)
+        self.pool = nn.MaxPool2d((2, 2))
+
+    def forward(self, inputs):
+        x = self.conv(inputs)
+        p = self.pool(x)
+
+        return x, p
+
+""" Decoder block:
+    The decoder block begins with a transpose convolution, followed by a concatenation with the skip
+    connection from the encoder block. Next comes the conv_block.
+    Here the number filters decreases by half and the height and width doubles.
+"""
+class decoder_block(nn.Module):
+    def __init__(self, in_c, out_c):
+        super().__init__()
+
+        self.up = nn.ConvTranspose2d(in_c, out_c, kernel_size=2, stride=2, padding=0)
+        self.conv = conv_block(out_c+out_c, out_c)
+
+    def forward(self, inputs, skip):
+        x = self.up(inputs)
+        x = torch.cat([x, skip], axis=1)
+        x = self.conv(x)
+
+        return x
+
+
+class build_unet(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        """ Encoder """
+        self.e1 = encoder_block(3, 64)
+        self.e2 = encoder_block(64, 128)
+        self.e3 = encoder_block(128, 256)
+        self.e4 = encoder_block(256, 512)
+
+        """ Bottleneck """
+        self.b = conv_block(512, 1024)
+
+        """ Decoder """
+        self.d1 = decoder_block(1024, 512)
+        self.d2 = decoder_block(512, 256)
+        self.d3 = decoder_block(256, 128)
+        self.d4 = decoder_block(128, 64)
+
+        """ Classifier """
+        self.outputs = nn.Conv2d(64, 1, kernel_size=1, padding=0)
+        
+        # NOTE: 
+        # nn.Conv2d(64, 1, kernel_size=1, padding=0) is mathematically same as 
+        # nn.Conv2d(64, 1, kernel_size=3, stride=1, padding=1)
+
+    def forward(self, inputs):
+        """ Encoder """
+        s1, p1 = self.e1(inputs)
+        s2, p2 = self.e2(p1)
+        s3, p3 = self.e3(p2)
+        s4, p4 = self.e4(p3)
+
+        """ Bottleneck """
+        b = self.b(p4)
+
+        """ Decoder """
+        d1 = self.d1(b, s4)
+        d2 = self.d2(d1, s3)
+        d3 = self.d3(d2, s2)
+        d4 = self.d4(d3, s1)
+
+        """ Classifier """
+        outputs = self.outputs(d4)
+        return outputs
+
+# Define model
+segmentation_model = build_unet()
+checkpoint = torch.load(model_path, map_location="cpu")
+segmentation_model.load_state_dict(checkpoint)
+# Send to GPU
+segmentation_model = segmentation_model.to(DEVICE) # runs on GPU
+segmentation_model.eval()
+
+def visualize(idx, **images):
+    """Plot images in one row.""" 
+    n = len(images)
+    fig = plt.figure(figsize=(20, 10))
+    for i, (name, image) in enumerate(images.items()):
+        plt.subplot(1, n, i + 1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.title(' '.join(name.split('_')).lower(), fontsize=20)
+        plt.imshow(image, cmap='gray')
+        plt.axis("off")
+        plt.show()
+
+        #plt.tight_layout()
+    #plt.savefig("../outputs/vis/compare-segs/{}.png".format(idx), facecolor="white", bbox_inches = 'tight')
+
+
+
+########### Hand segmentation model ###########
+
+# Create the model
+hand_seg_model = torch.hub.load(
+    repo_or_dir='guglielmocamporese/hands-segmentation-pytorch', 
+    model='hand_segmentor', 
+    pretrained=True
+)
+
+hand_seg_model.eval() # runs on CPU
+
+
+transforms_image = transforms.Compose([transforms.Resize((224, 224)),
+                                          transforms.CenterCrop((224, 224)),
+                                          transforms.ToTensor(),
+                                          transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+
+########### Entropy masking ###########
+
+def entropy_based_seg(img):
+    image_gray = rgb2gray(img)
+    image_gray = img_as_ubyte(image_gray)
+    entropy_image = entropy(image_gray, disk(6)) # 6 default
+    scaled_entropy = entropy_image / entropy_image.max()
+    threshold = scaled_entropy > 0.8 # 0.8 default
+    image_seg = np.dstack([img[:,:,0]*threshold,
+                            img[:,:,1]*threshold,
+                            img[:,:,2]*threshold])
+    return image_seg
+
+
+## This is the final segmentation step we use for our approach
+def crop_with_hand_entropy_seg(img):
+    """
+    Crops product image using product, hand and entropy masking based segmentation.
+    
+    Reference: https://towardsdatascience.com/image-processing-with-python-working-with-entropy-b05e9c84fc36
+    """
+        
+    image_b = torch.unsqueeze(img, 0).to(img) # same data type as the variable 'img'
+    image = (image_b[0].permute(1,2,0).detach().cpu().numpy()+1)/2
+    image = (image*255).astype(np.uint8)
+    pred = hand_seg_model(image_b).argmax(1).detach().cpu().numpy().squeeze()
+    # invert mask, use plt.imshow(hand_pred) for vis
+    hand_pred = np.logical_not(pred).astype(int) 
+    
+    # Stack preds for getting ROI
+    hand_pred3d = np.stack((hand_pred, hand_pred, hand_pred), axis=-1)
+    # Get rid off hands
+    image_no_hands = np.array(image) * hand_pred3d # use plt.imshow(image_no_hands) for vis
+    
+    # Convert new image to tensor
+    image_no_hands = Image.fromarray(np.uint8(image_no_hands)).convert('RGB')
+    image_tensor = transforms_image(image_no_hands)
+
+    image_tensor_b = torch.unsqueeze(image_tensor, 0).to(DEVICE)
+    output = torch.sigmoid(segmentation_model(image_tensor_b.float()))
+    pred = output.permute(0, 2, 3, 1).squeeze().detach().cpu().numpy() > 0.5
+    
+    # Stack preds for getting ROI
+    mask = np.stack((pred, pred, pred), axis=-1)
+    
+    # Image ROI
+    image_roi = image_no_hands * mask
+    
+    image_roi = entropy_based_seg(image_roi)
+    
+    data = {"roi": image_roi,
+            "mask": pred,
+            "image": image
+           }
+    
+    return data
--- training/segmentation/README.md
+++ training/segmentation/README.md
@@ -1,17 +1,15 @@
-# Segment objects from synthetic images
+# this sub-folder is for the segmentation pipeline for our approach
 
 This code requires Python 3.8.12 and PyTorch 1.8.2.
 
 To use this code:
 
-* clone repo
-* open a folder `datasets` in the root folder and keep `Auto-retail-syndata-release` inside it
-* run `train_segmentation.ipynb` which trains U-Net
-* run `test_segmentation.ipynb` for making predictions and visualization on validation set using trained model
+* open a folder `datasets` in the root folder and keep `Auto-retail-syndata-release` inside it, see dataloader for details
+* run `training/train_segmentation.py` which trains U-Net
+* see `test_segmentation.ipynb` for making predictions and visualization on validation set using trained model
+* see `test_segmentation.ipynb` for making predictions and visualization on validation set using trained model and our post processing approach
 
-note on running `test_segmentation.ipynb`: either run your trained model or get pretrained weights from [here](https://github.com/acc-track-4/product-segmentation/releases/tag/v0.0.1). If you use the pretrained model, open a folder `logs` in root dir and keep it there.
+Note on running `test_segmentation.ipynb`: either run your trained model or get pretrained weights from [here](https://github.com/istiakshihab/automated-retail-checkout-aicity22/releases/tag/v0.0.1). If you use the pretrained model, open a folder `logs` in root dir and keep it there.
 
-
-Todo: 
-* inference (give one image/frame, get mask)
+The main output from this sub-folder is the `*.pth` file of the trained U-Net model which is to be used in the final testing stage.
 
